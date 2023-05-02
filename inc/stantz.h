10fadd0035d1c341786a71b58054ad24647233c1
Tristan Slater <hello@trslater.ca>
2023-05-02 08:30:31 -0700

    Write arbitrary pixel colors to window

--- inc/stantz.h
+++ inc/stantz.h
@@ -3,6 +3,10 @@
 
 #include <SDL2/SDL.h>
 
+typedef double ColorRGB[3];
+
 void render( int, int );
+void set_pixel( SDL_Surface *, int, int, ColorRGB );
+Uint32 rgb_to_int( SDL_PixelFormat *, ColorRGB );
 
 #endif
--- src/stantz.c
+++ src/stantz.c
@@ -1,5 +1,6 @@
 #include "stantz.h"
 
+// SDL rendering based on https://lazyfoo.net/tutorials/SDL/02_getting_an_image_on_the_screen/index.php
 void render( int width, int height )
 {
     if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
@@ -22,6 +23,24 @@ void render( int width, int height )
     
     SDL_Surface *surface = SDL_GetWindowSurface( window );
 
+    ColorRGB pixels[width*height];
+
+    // Render to screen
+    for( int i = 0; i < height; ++i )
+    {
+        for( int j = 0; j < width; ++j )
+        {
+            double t = (double)(height - i)/(double)height;
+            double s = (double)(width - j)/(double)width;
+
+            pixels[i*width + j][0] = s;
+            pixels[i*width + j][1] = t;
+            pixels[i*width + j][2] = t;
+
+            set_pixel( surface, i, j, pixels[i*width + j] );
+        }
+    }
+
     SDL_UpdateWindowSurface( window );
 
     // Hack to get window to stay up
@@ -37,4 +56,20 @@ void render( int width, int height )
 
     SDL_DestroyWindow( window );
     SDL_Quit();
+}
+
+void set_pixel( SDL_Surface *surface, int i, int j, ColorRGB c )
+{
+    Uint32 * const pixel = ( Uint32 * )(
+        ( Uint8 * ) surface->pixels
+        + i*surface->pitch
+        + j*surface->format->BytesPerPixel
+    );
+
+    *pixel = rgb_to_int( surface->format, c );
+}
+
+Uint32 rgb_to_int( SDL_PixelFormat *format, ColorRGB c )
+{
+    return SDL_MapRGB( format, 255*c[0], 255*c[1], 255*c[2] );
 }
