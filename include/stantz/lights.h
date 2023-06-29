20ed21b0340737f973077f4acfc6a4bfa449148a
Tristan Slater <hello@trslater.ca>
2023-06-29 12:45:12 -0700

    Convert lights to use Eigen
    This also required streamlining.

--- include/stantz/lights.h
+++ include/stantz/lights.h
@@ -1,23 +1,8 @@
 #pragma once
 
-#include <stdlib.h>
-
 #include <Eigen/Dense>
 
-#include "linalg.h"
-#include "materials.h"
-
 struct Light {
-    Vector3D position;
-    ColorRGB color;
+    Eigen::Vector3d position;
+    Eigen::Vector3d color;
 };
-
-struct LightList {
-    Light **items;
-    int capacity;
-    int count;
-};
-
-void init_light_list( LightList *, int );
-int light_list_append( LightList *, Light * );
-void destroy_light_list( LightList * );
