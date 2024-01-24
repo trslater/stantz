4064d97b4c553aae7b7bb2de32489d8a2a222283
trslater <hello@trslater.ca>
2024-01-23 22:11:31 -0800

    Start from scratch

--- include/stantz/cameras.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <Eigen/Dense>
-
-struct Camera {
-    Eigen::Vector3d origin;
-    double fov;
-    double focal_length;
-};
--- include/stantz/lights.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <Eigen/Dense>
-
-struct Light {
-    Eigen::Vector3d position;
-    Eigen::Vector3d color;
-};
