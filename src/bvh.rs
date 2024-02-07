01f8be740ded8a4fdd74b7b6d1b8e2f9ff393f31
trslater <hello@trslater.ca>
2024-02-06 20:49:00 -0800

    Move AABB to geometry module

--- src/bvh.rs
+++ src/bvh.rs
@@ -2,20 +2,6 @@ use crate::geometry::Geometry;
 use crate::materials::Material;
 
 #[derive(Debug)]
-pub struct AABB {
-    pub left: f32,
-    pub right: f32,
-    pub bottom: f32,
-    pub top: f32,
-    pub back: f32,
-    pub front: f32,
-}
-
-pub trait MakeAABB {
-    fn make_aabb(&self) -> AABB;
-}
-
-#[derive(Debug)]
 pub struct BVH<'a> {
     pub root: BVHNode<'a>,
 }
@@ -23,7 +9,6 @@ pub struct BVH<'a> {
 #[derive(Debug)]
 // Ideally, object should only be Some when both children are None
 pub struct BVHNode<'a> {
-    pub aabb: AABB,
     pub object: Option<&'a (Geometry, Material)>,
     pub left_child: Option<Box<Self>>,
     pub right_child: Option<Box<Self>>,
--- src/geometry.rs
+++ src/geometry.rs
@@ -1,7 +1,5 @@
 use na::{Matrix3, Vector3};
 
-use crate::bvh::{MakeAABB, AABB};
-
 use ray::Ray;
 
 pub mod ray {
@@ -41,6 +39,10 @@ pub enum Geometry {
         b: Vector3<f32>,
         c: Vector3<f32>,
     },
+    AABox {
+        min: Vector3<f32>,
+        max: Vector3<f32>,
+    },
 }
 
 impl Geometry {
@@ -52,6 +54,9 @@ impl Geometry {
                 [a.y, b.y, c.y].iter().sum::<f32>() / 3.0,
                 [a.z, b.z, c.z].iter().sum::<f32>() / 3.0,
             ),
+            Geometry::AABox { min, max } => {
+                Vector3::new(max.x - min.x, max.y - min.y, max.z - min.z)
+            }
         }
     }
 
@@ -59,6 +64,46 @@ impl Geometry {
         match self {
             Geometry::Sphere { center, .. } => (point - center).normalize(),
             Geometry::Triangle { a, b, c } => (b - a).cross(&(c - a)).normalize(),
+            Geometry::AABox { min, max } => {
+                // TODO: Implement edge/corner cases (literally)
+                let is_left_of = point.x <= min.x;
+                let is_right_of = point.x >= max.x;
+                let is_below = point.y <= min.y;
+                let is_above = point.y >= max.y;
+                let is_behind = point.z <= min.z;
+                let is_in_front_of = point.z >= max.z;
+
+                let is_in_x_bounds = point.x >= min.x && point.x <= max.x;
+                let is_in_y_bounds = point.y >= min.y && point.y <= max.y;
+                let is_in_z_bounds = point.z >= min.z && point.z <= max.z;
+
+                if is_in_x_bounds && is_in_y_bounds {
+                    if is_behind {
+                        return Vector3::new(0.0, 0.0, -1.0);
+                    }
+                    if is_in_front_of {
+                        return Vector3::new(0.0, 0.0, 1.0);
+                    }
+                }
+                if is_in_x_bounds && is_in_z_bounds {
+                    if is_below {
+                        return Vector3::new(0.0, -1.0, 0.0);
+                    }
+                    if is_above {
+                        return Vector3::new(0.0, 1.0, 0.0);
+                    }
+                }
+                if is_in_y_bounds && is_in_z_bounds {
+                    if is_left_of {
+                        return Vector3::new(-1.0, 0.0, 0.0);
+                    }
+                    if is_right_of {
+                        return Vector3::new(1.0, 0.0, 0.0);
+                    }
+                }
+
+                Vector3::new(0.0, 0.0, 0.0)
+            }
         }
     }
 
@@ -107,20 +152,49 @@ impl Geometry {
                     })
                     .map(|[.., t]| t)
             }
+
+            Geometry::AABox { min, max } => {
+                let left_t = (min.x - ray.origin.x) / ray.direction().x;
+                let right_t = (max.x - ray.origin.x) / ray.direction().x;
+                let bottom_t = (min.y - ray.origin.y) / ray.direction().y;
+                let top_t = (max.y - ray.origin.y) / ray.direction().y;
+                let back_t = (min.z - ray.origin.z) / ray.direction().z;
+                let front_t = (max.z - ray.origin.z) / ray.direction().z;
+
+                let t_min = left_t
+                    .min(right_t)
+                    .max(bottom_t.min(top_t))
+                    .max(back_t.min(front_t));
+
+                let t_max = left_t
+                    .max(right_t)
+                    .min(bottom_t.max(top_t))
+                    .min(back_t.max(front_t));
+
+                if t_max < 0.0 {
+                    return None;
+                }
+
+                // To get here, t_max >= 0
+                if t_min > t_max {
+                    // Then t_min >= 0 here
+                    return None;
+                }
+
+                if t_min < 0.0 {
+                    return Some(t_max);
+                }
+
+                Some(t_min)
+            }
         }
     }
-}
 
-impl MakeAABB for Geometry {
-    fn make_aabb(&self) -> AABB {
+    pub fn aabb(&self) -> Geometry {
         match self {
-            Geometry::Sphere { center, radius } => AABB {
-                left: center.x - radius,
-                right: center.x + radius,
-                bottom: center.y - radius,
-                top: center.y + radius,
-                back: center.z - radius,
-                front: center.z + radius,
+            Geometry::Sphere { center, radius } => Geometry::AABox {
+                min: Vector3::new(center.x - radius, center.y - radius, center.z - radius),
+                max: Vector3::new(center.x + radius, center.y + radius, center.z + radius),
             },
             Geometry::Triangle { a, b, c } => {
                 // TODO: See if this can be done immutably
@@ -131,15 +205,16 @@ impl MakeAABB for Geometry {
                 let mut zs = [a.z, b.z, c.z];
                 zs.sort_by(|a, b| a.total_cmp(b));
 
-                AABB {
-                    left: xs[0],
-                    right: xs[2],
-                    bottom: ys[0],
-                    top: ys[2],
-                    back: zs[0],
-                    front: zs[2],
+                Geometry::AABox {
+                    min: Vector3::new(xs[0], ys[0], zs[0]),
+                    max: Vector3::new(xs[2], ys[2], zs[2]),
                 }
             }
+            // TODO: Figure out best way to hand this
+            Geometry::AABox { min, max } => Geometry::AABox {
+                min: *min,
+                max: *max,
+            },
         }
     }
 }
